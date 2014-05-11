06d271758b1c9d5bcb5975f4424a07567f23abc7
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-05-11 14:55:02 +0100

    Removed $SPARK_HOME and the jar location, we're not on a cluster yet.

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -3,9 +3,8 @@ import org.apache.spark.SparkContext._
 
 object SimpleApp {
   def main(args: Array[String]) {
-    val sc = new SparkContext("local", "Simple App", "$SPARK_HOME",
-        List("target/scala-2.10/spark-simple-app_2.10-1.0.jar"))
     val textFile = "/etc/hosts" // Should be some file on your system
+    val sc = new SparkContext("local", "Simple App")
     val logData = sc.textFile(textFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
