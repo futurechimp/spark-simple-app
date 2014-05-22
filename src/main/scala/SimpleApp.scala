fde5d7150345c7bc42b343ec1ce3a97a54935717
Dave Hrycyszyn <dave.hrycyszyn@headlondon.com>
2014-05-22 15:50:16 +0100

    Fixing mistaken comment.

--- src/main/scala/SimpleApp.scala
+++ src/main/scala/SimpleApp.scala
@@ -6,7 +6,8 @@ object SimpleApp {
     val sc = new SparkContext("spark://yourhostname:7077",    // CHANGED to spark url
     "Simple App",                                             // stays the same
     "/path/to/spark-0.9.1-bin-hadoop1/",                      // CHANGED to spark path
-    List("target/scala-2.10/spark-simple-app_2.10-1.0.jar"))  // this is new    val logData = sc.textFile(textFile, 2).cache()
+    List("target/scala-2.10/spark-simple-app_2.10-1.0.jar"))  // this is new
+    val logData = sc.textFile(textFile, 2).cache()
     val numAs = logData.filter(line => line.contains("a")).count()
     val numBs = logData.filter(line => line.contains("b")).count()
     println("Lines with a: %s, Lines with b: %s".format(numAs, numBs))
